7813e518ea4fd3765e8d02e25c1a4bab5f552023
makotot <makoto.ttn@gmail.com>
2015-11-22 17:23:46 +0900

    + loadVideoById

---
+++ src/js/video-list.js
@@ -0,0 +1,43 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var apiUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&key=AIzaSyDxpcGtEL7bMKiyQDjBjHXTjZXuZbs2ppk&type=video&q=';
+document.querySelector('#form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  var query = document.querySelector('#search').value;
+
+  window.fetch(apiUrl + query)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log(data.items[0].id.videoId)
+      window.player.loadVideoById(data.items[0].id.videoId)
+    })
+});
+
+window.player;
+
+window.onYouTubeIframeAPIReady = function () {
+  window.player = new YT.Player('player', {
+    videoId: 'M7lc1UVf-VE',
+    width: 300,
+    height: 200,
+    events: {
+      onReady: function (event) {
+        console.log(event)
+      },
+      onStateChange: function (event) {
+        if (event.data === YT.PlayerState.ENDED) {
+          console.log(event.data)
+        }
+      },
+      onError: function (err) {
+        console.log(err)
+      }
+    }
+  })
+}
